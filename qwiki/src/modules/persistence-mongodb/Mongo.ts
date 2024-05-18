8c41e130853e58b8ae03b3ada7c2d41a6be4e75d
qooando <gryfonheart@gmail.com>
2024-05-18 19:57:09 +0200

    feat: Mongo bean init

--- qwiki/src/modules/persistence-mongodb/Mongo.ts
+++ qwiki/src/modules/persistence-mongodb/Mongo.ts
@@ -1,22 +1,27 @@
 import {NoSqlPersistence} from "@qwiki/modules/persistence/NoSqlPersistence";
-import {Db as MongoDb} from "mongodb";
+import {Db as MongoDb, MongoClient} from "mongodb";
 import {Value} from "@qwiki/core/beans/Value";
 import {__Bean__} from "@qwiki/core/beans/__Bean__";
 import {Objects} from "@qwiki/core/utils/Objects";
 import {NotImplementedException} from "@qwiki/core/utils/Exceptions";
 
+/**
+ * https://www.mongodb.com/resources/products/compatibilities/using-typescript-with-mongodb-tutorial
+ */
 export class Mongo extends NoSqlPersistence {
     static __bean__: __Bean__ = {
         loadCondition: () => Objects.getValueOrDefault($qw.config, "qwiki.persistence.mongodb", false)
     }
 
-    db: Mongo
+    db: MongoDb
+    client: MongoClient
+    dbName: string = Value("qwiki.persistence.mongodb.connectionString", "default")
     dbConnectionString: string = Value("qwiki.persistence.mongodb.connectionString", "not_defined")
 
     async postConstruct() {
         this.log.debug(`MongoDB at: ${this.dbConnectionString}`)
-        // FIXME
-        throw new NotImplementedException();
-        // this.db =
+        this.client = new MongoClient(this.dbConnectionString);
+        await this.client.connect();
+        this.db = this.client.db(this.dbName);
     }
 }
